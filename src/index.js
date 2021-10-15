70718f3407e8cfc33b3f04f9b68e8f16e495a7b1
IhorShvydkyi <igorshvydky35@gmail.com>
2021-10-15 18:15:05 +0300

    before deploy

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-import './styles.css';
-import menu from './menu.json';
+// импорт меню, хбс, стилей
 import menuTpls from '../src/templates/menuTpls.hbs';
+import menu from './menu.json';
+import './styles.css';
+
 
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
 
+// создаем рефы
 const refs = {
   body: document.querySelector('body'),
   themeChange: document.querySelector('#theme-switch-toggle'),
   menu: document.querySelector('.js-menu'),
 }
-
 refs.themeChange.addEventListener('change', OnThemeChange);
 
+// скрипт для дефолтной темы
+themeDefault()
+
+function themeDefault() {
+  if (!localStorage.length) {
+      localStorage.setItem('theme', Theme.LIGHT)
+  };
+  refs.body.classList.add(localStorage.getItem('theme'));
+  
+  if (refs.body.classList.contains(Theme.DARK)) {
+      refs.themeChange.checked = true;
+  }
+}
+
+// скрипт для переключателя
 function OnThemeChange(){
   refs.body.classList.toggle(Theme.DARK);
 
@@ -25,6 +42,10 @@ function OnThemeChange(){
   }
 }
 
+
+
+
+// разметка на сайт
 const cardsMarkup = createCardsMenuMarkup(menu);
 
 refs.menu.insertAdjacentHTML('beforeend', cardsMarkup);
